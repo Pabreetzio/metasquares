5e1dd324f23e8be7d33276bf361e991b01f37e20
Patrick Graham <pabreetzio@gmail.com>
2026-01-16 15:31:42 -0500

    chore: remove test import file

--- apps/mobile/test-import.js
+++
@@ -1,7 +0,0 @@
-// Quick test to see if imports resolve
-try {
-  const shared = require('../../packages/shared/src/index.ts');
-  console.log('✓ Shared package imports:', Object.keys(shared));
-} catch (e) {
-  console.error('✗ Failed to import shared package:', e.message);
-}
